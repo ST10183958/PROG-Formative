1467fef452466373ab5870c9816521c46f4905a6
ST10183958 <128601617+ST10183958@users.noreply.github.com>
2025-04-23 21:25:21 +0900

    Update Program.cs

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -2,6 +2,46 @@
 using System.Media;
 using Part1;
 
+/*
+Referencing
+*****************************************************
+Title: Thread.Sleep Method
+Author: Microsoft
+Date: 2025
+Code version: .NET 8
+Availability: <https://learn.microsoft.com/en-us/dotnet/api/system.threading.thread.sleep?view=net-9.0>
+*****************************************************
+Title: Exceptions Try..Catch
+Author: W3school
+Date: n/d
+Code version: .NET 8
+Availability:<https://www.w3schools.com/cs/cs_exceptions.php>
+*****************************************************
+Title: Convert.ToString Method
+Author: Microsoft
+Date: 2025
+Code version: .NET 8
+Availability: <https://learn.microsoft.com/en-us/dotnet/api/system.convert.tostring?view=net-9.0>
+*****************************************************
+Title: C# Classes and Objects
+Author: W3schools
+Date: n/d
+Code version: .NET 8
+Availability:<https://www.w3schools.com/cs/cs_classes.php>
+*****************************************************
+Title: How to change Foreground Color of Text in Console
+Author: Geeksforgeeks
+Date: 2019
+Code version: .NET 8
+Availability: <https://www.geeksforgeeks.org/c-sharp-how-to-change-foreground-color-of-text-in-console/>
+*****************************************************
+Title: Figgle
+Author: Noakes. D
+Date: 2023
+Code version: .NET 8
+Availability: <https://github.com/drewnoakes/figgle>
+*****************************************************
+*/
 
 namespace FormativePROG
 {
